18a4c384c6d1db49e03f272fde99ed11150147d8
johnGachihi <johngachihi3@gmail.com>
2020-10-24 12:37:57 +0300

    Make Event classes implementors of the ShouldBroadcast interface

--- app/Events/PeerConnectionICE.php
+++ app/Events/PeerConnectionICE.php
@@ -10,7 +10,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class PeerConnectionICE
+class PeerConnectionICE implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
